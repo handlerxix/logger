7aec0a3895b2ec6a7ea229cf5619951ada8e9e12
Dmitriy Demin <dmitriy.demin@corp.mail.ru>
2021-10-19 18:25:46 +0300

    Entering log-strings

--- application/src/main/java/org/example/demin/Application.java
+++ application/src/main/java/org/example/demin/Application.java
@@ -4,12 +4,19 @@ import com.google.inject.Guice;
 import org.example.demin.log.LoggerBase;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Scanner;
+
 public final class Application {
   public static void main(@NotNull String[] args) {
     final var injector = Guice.createInjector(new TestModule(args));
 
     final var logger = injector.getInstance(LoggerBase.class);
-    logger.log("Keks");
-    logger.log("Ukrop");
+
+    final var reader = new Scanner(System.in);
+
+    for (int i = 0; i < 10; i++) {
+      System.out.print("\nEnter log-string: ");
+      logger.log(reader.nextLine());
+    }
   }
 }
